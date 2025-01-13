6830758a262e6abaca0af8064be66b50fec83b53
Filipe Costa <filipe@rhizatech.com.br>
2025-01-13 14:52:59 -0300

    Files from watch and computed properties

--- section-2/app.js
+++ section-2/app.js
@@ -1,11 +1,32 @@
-const App = Vue.createApp(
-    {
-        data() {
-            return {
-                courseGoal: 'Finish the course and learn Vue',
-            }
+const App = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: "Finish the course and learn Vue",
+      courseGoalB: "Master the course and learn Vue",
+      vueLink: "https://athlos.gg",
+      name: 'Pepeto'
+    };
+  },
+  computed: {
+    fullName() {
+        return this.name + ' Carvalho'
+    }
+  },
+  watch: {
+    name(value) {
+        this.fullname = value + ' Carvalho';
+    }
+  },
+  methods: {
+    outputGoal() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+            return this.courseGoalA;
+        } else {
+            return this.courseGoalB;
         }
     }
-);
+  }
+});
 
-App.mount('#user-goal');
+App.mount("#user-goal");
