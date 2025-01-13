2f1c225a38c20871b041c078e284d1fb7eb9825b
Filipe Costa <filipe@rhizatech.com.br>
2025-01-13 11:29:14 -0300

    Creating the first Vue3 app from scratch

---
+++ section-2/app.js
@@ -0,0 +1,11 @@
+const App = Vue.createApp(
+    {
+        data() {
+            return {
+                courseGoal: 'Finish the course and learn Vue',
+            }
+        }
+    }
+);
+
+App.mount('#user-goal');
